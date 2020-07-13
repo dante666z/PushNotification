ef387d662b351c0b752f3b0825d289c7fdd45531
Zuriel Zarate <mdantez666@gmail.com>
2020-07-13 00:06:02 -0500

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Push from 'push.js';
+
+class Name extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    Push.create('Hello World!');
+  }
+  render() {
+    return <button onClick={this.handleClick}>Clickéame</button>;
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Name></Name>
   );
 }
 
